1600c8f9d5517c54d6c632769f508eec454f96d2
Macnelson9 <michaelofatu@gmail.com>
2025-02-13 19:50:04 +0100

    Implement and Improve user interface and experience

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selected elements
+const userInput = document.getElementById('email__input').value;
+const subscribeBtn = document.getElementById('success__page');
+const errorEl = document.querySelector('.error');
+
+subscribeBtn.addEventListener('click', function () {
+  alert('I was clicked!!');
+});
